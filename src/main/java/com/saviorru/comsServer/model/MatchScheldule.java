0d6e8cbe1ff6d9f4d2ce6672bb3683666bc302b2
SAVIORRU <stas011298@gmail.com>
2018-02-22 14:33:39 +0300

    Complete schemeGenerator, tournament interface

--- src/main/java/com/saviorru/comsServer/model/MatchScheldule.java
+++
@@ -1,8 +0,0 @@
-package com.saviorru.comsServer.model;
-
-import java.util.HashMap;
-
-public interface MatchScheldule<T> {
-
-    HashMap<T,Match> getMatchScheldule();
-}
--- src/main/java/com/saviorru/comsServer/model/generators/Round.java
+++ src/main/java/com/saviorru/comsServer/model/generators/Round.java
@@ -63,6 +63,6 @@ public class Round implements SchelduleGenerator {
 
     @Override
     public TypeScheme getGeneratorType() {
-        return null;
+        return this.type;
     }
 }
