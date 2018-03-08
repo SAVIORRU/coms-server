f9e5f935978c8fb841de802d3749ba1cc454be00
SAVIORRU <stas011298@gmail.com>
2018-03-08 15:03:39 +0300

    update

--- src/main/java/com/saviorru/comsserver/model/SchelduleController.java
+++ src/main/java/com/saviorru/comsserver/model/SchelduleController.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 public interface SchelduleController {
     public void createScheldule(List<Player> players);
-    public void updateScheldule(List<Player> players);
-    public void setMatchResult(Match match);
+    public void updateScheldule();
     public List<Match> getMatchesByPlayer(Player player, StateMatch state);
     public List<Match> getMatchesByDate(LocalDate date);
     public List<Match> getMatchesByLocation(Location location);
