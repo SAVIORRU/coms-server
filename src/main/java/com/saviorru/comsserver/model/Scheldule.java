b9a0406c6073eeff9794a5fc255c675a79735e36
SAVIORRU <stas011298@gmail.com>
2018-03-08 13:49:35 +0300

    no message

--- src/main/java/com/saviorru/comsserver/model/Scheldule.java
+++ src/main/java/com/saviorru/comsserver/model/Scheldule.java
@@ -1,4 +1,9 @@
 package com.saviorru.comsserver.model;
 
+import java.util.List;
+
 public interface Scheldule {
+    public List<Match> getAllMatches();
+    public List<Match> getMatchesByState(StateMatch state);
+    public void finishMatch(Match match);
 }
--- src/main/java/com/saviorru/comsserver/model/SchelduleController.java
+++ src/main/java/com/saviorru/comsserver/model/SchelduleController.java
@@ -1,4 +1,13 @@
 package com.saviorru.comsserver.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SchelduleController {
+    public void createScheldule(List<Player> players);
+    public void updateScheldule(List<Player> players);
+    public void setMatchResult(Match match);
+    public List<Match> getMatchesByPlayer(Player player, StateMatch state);
+    public List<Match> getMatchesByDate(LocalDate date);
+    public List<Match> getMatchesByLocation(Location location);
 }
