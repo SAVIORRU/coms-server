e91587b1c0089b3ec7a80882618870d0c3ef561a
DESKTOP-3O6EG65\HOME-PC <Gallifrey46@gmail.com>
2018-03-14 15:42:27 +0300

    Изменение возращаемого значения в методах интерфейса Scheme

--- src/main/java/com/saviorru/comsserver/domain/Main.java
+++ src/main/java/com/saviorru/comsserver/domain/Main.java
@@ -1,11 +1,14 @@
 package com.saviorru.comsserver.domain;
 
+import javafx.util.Pair;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
+        // in = new Scanner(System.in);
         //System.out.println(in.nextLine());
-        if(in.nextLine().equalsIgnoreCase( "Help me")) System.out.println("Can are you help ?");
+        //if(in.nextLine().equalsIgnoreCase( "Help me")) System.out.println("Can are you help ?");
+        //HashMap<Integer,ArrayList<ArrayList<Player>>>
     }
 }
--- src/main/java/com/saviorru/comsserver/domain/Scheme.java
+++ src/main/java/com/saviorru/comsserver/domain/Scheme.java
@@ -1,13 +1,15 @@
 package com.saviorru.comsserver.domain;
 
+import javafx.util.Pair;
+
 import java.util.List;
 
 public interface Scheme {
 
-    Player getCurrentTourContestant(Player player);
-    List<Player> getAllContestant(Player player);
-    List<Player> getPlayersCurrentTour();
-    List<Player> getPlayersTheTour(int tourNumber);
+    Integer getNumberContestantCurrentTour(Integer playerNumber);
+    List<Integer> getAllContestant(Integer playerNumber);
+    List<Pair<Integer,Integer>> getPlayersNumbersCurrentTour();
+    List<Pair<Integer,Integer>> getPlayersNumbersTheTour(int tourNumber);
     int getCountTour();
 
 }
