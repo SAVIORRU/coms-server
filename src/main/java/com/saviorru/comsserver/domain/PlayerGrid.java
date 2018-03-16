44f20929c4a0e7a32a583a6e68701bb5af3a18fa
Vitaliy <Gallifrey46@gmail.com>
2018-03-17 01:08:18 +0300

    тестовый вариант отрисовки дерева

---
+++ src/main/java/com/saviorru/comsserver/domain/PlayerGrid.java
@@ -0,0 +1,11 @@
+package com.saviorru.comsserver.domain;
+
+import javafx.util.Pair;
+
+import java.util.List;
+
+public interface PlayerGrid {
+
+    List<Pair<Integer, Integer>> getPlayerTheRound();
+
+}
