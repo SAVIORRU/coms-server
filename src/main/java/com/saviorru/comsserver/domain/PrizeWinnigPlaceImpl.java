15ad65a1b149be1e4a2b7b501b3a7820e6e022ce
Vitaliy <Gallifrey46@gmail.com>
2018-03-19 17:03:52 +0300

    Добавлены методы в интерфейс PrizeWinnigPlace и создана его реализация

---
+++ src/main/java/com/saviorru/comsserver/domain/PrizeWinnigPlaceImpl.java
@@ -0,0 +1,29 @@
+package com.saviorru.comsserver.domain;
+
+public class PrizeWinnigPlaceImpl implements PrizeWinningPlace {
+
+    @Override
+    public Player getFirstPrizePlace() {
+        return null;
+    }
+
+    @Override
+    public Player getSecondPrizePlace() {
+        return null;
+    }
+
+    @Override
+    public Player getThridPrizePlace() {
+        return null;
+    }
+
+    @Override
+    public Player getPlayerThePrizePlace(int PlacePrize) {
+        return null;
+    }
+
+    @Override
+    public int getCountPrizePlace() {
+        return 0;
+    }
+}
--- src/main/java/com/saviorru/comsserver/domain/PrizeWinningPlace.java
+++ src/main/java/com/saviorru/comsserver/domain/PrizeWinningPlace.java
@@ -2,5 +2,10 @@ package com.saviorru.comsserver.domain;
 
 public interface PrizeWinningPlace {
 
-    
+    Player getFirstPrizePlace();
+    Player getSecondPrizePlace();
+    Player getThridPrizePlace();
+    Player getPlayerThePrizePlace(int PlacePrize);
+    int getCountPrizePlace();
+
 }
